4fa99630c2a28581999916648f988e55468913e0
Money <xxmoneyman8902xx@gmail.com>
2018-01-12 20:43:18 -0600

    Finalized the Drive Train, began on the Pneumatic

--- 2018-main/src/Robot.cpp
+++ 2018-main/src/Robot.cpp
@@ -8,36 +8,67 @@
 #include <memory>
 #include <string>
 #include "WPILib.h"
-#include "Victor.h"
 #include <IterativeRobot.h>
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
 #include "RobotDrive.h"
 #include "XboxController.h"
+#include "ctre/Phoenix.h"
+#include "MotorSafetyHelper.h"
+#include <DoubleSolenoid.h>
 class Robot: public frc::IterativeRobot {
-	//Motor controllers set up
-	Victor * 	pclDriveMotorL;
-	Victor *	pclDriveMotorR;
+	const int PDP = 1;
+	const int PCM = 2;
 
-	RobotDrive *	pclRobotDrive;
-	//XboxController* 	pclXbox;
+	//Set up Drive trains
+	MecanumDrive *m_robotDrive;
+
+	//set up controllers
+	//Using the two Xbox controllers for competition the Joystick is for potential change in the future todays date is 1/12/18
+	XboxController* 	pclXbox;
+	XboxController* 	pclXbox2;
 	Joystick *	pclJoystick;
+
+
+	//set up motor controllers
+	//If you need to set up TalonSrx's in the future copy one of these and chanfe the device number found in the ()
+	WPI_TalonSRX *lf = new WPI_TalonSRX(0); /*left front */
+	WPI_TalonSRX *lr = new WPI_TalonSRX(1);/*left rear */
+	WPI_TalonSRX *rf = new WPI_TalonSRX(2); /*right front */
+	WPI_TalonSRX *rr = new WPI_TalonSRX(3); /*right rear */
+
+	//Setting up solenoid for potential climbing or cube placement on the robot
+	//Should be really easy to change for POWER UP robot in the future if something changes
+	DoubleSolenoid *	pclSolenoid;
 
 
 public:
 	Robot()
 		{
-			//Initial Drive controllers
-			pclDriveMotorR = new Victor(1);
-			pclDriveMotorL = new Victor(0);
+				//reset motor safety timeout//
+				lf->Set(ControlMode::PercentOutput, 0);
+				lr->Set(ControlMode::PercentOutput, 0);
+				rf->Set(ControlMode::PercentOutput, 0);
+				rr->Set(ControlMode::PercentOutput, 0);
 
-			//Initial the robot drive
-			pclRobotDrive	= new RobotDrive(pclDriveMotorR,pclDriveMotorL);
+				//Initial the robot drive
+				//Sets the different motor controllers for the drivebase
+				m_robotDrive = new MecanumDrive(*lf, *lr, *rf, *rr);
+				//sets the safety time out for the motor safety feature on the motors (This is required by FIRST)
+				m_robotDrive->SetExpiration(0.5);
+				//This is for Power consumption and is not as important but still needs to be in the code
+				m_robotDrive->SetSafetyEnabled(false);
 
-			//Initial the joy-stick and inputs
-			//pclXbox = new XboxController(0);
+			//Initial the joy-stick and inputs as well as the Xbox controllers
+			//The values found in the () are for the USB ports that you can change in the Driver Station
+			pclXbox = new XboxController(0);
+			pclXbox2 = new XboxController(1);
+			//The Joystick is for the potential of the future and not for the robot  as of right now 1/12/18
 			pclJoystick = new Joystick(0);
+
+			//Setting up Pneumatic
+			pclSolenoid = new DoubleSolenoid(PCM,1,0);
 
 
 		}
@@ -62,6 +93,7 @@ public:
 	 * well.
 	 */
 	void AutonomousInit() override {
+
 		m_autoSelected = m_chooser.GetSelected();
 		// m_autoSelected = SmartDashboard::GetString(
 		// 		"Auto Selector", kAutoNameDefault);
@@ -85,7 +117,16 @@ public:
 	void TeleopInit() {}
 
 	void TeleopPeriodic() {
-		pclRobotDrive->ArcadeDrive(pclJoystick,0);
+		//driving robot in teleop phase
+		/*When programming the drive base you need to call the drivebase that you named in the pointer above.
+		//When asigning values it depends on the drive train that you define above and the the subtype, I'm not sure exactly what to call it but in this instace it is DriveCartesian
+		//The different values can be set or set with controllers depending on when in the match it is
+		//The Different joysticks on the controller can be defined like it is below, something to consider though, the way the WPIlib says to set up a drivetrain like this you ddo GetY the GetX
+		//But this is backward. And the foward and backward is inversed so multiplying the joystick value by -1 should fix all errors with this issue*/
+		m_robotDrive->DriveCartesian(pclXbox->GetX(frc::XboxController::kLeftHand),pclXbox->GetY(frc::XboxController::kLeftHand)*-1,pclXbox->GetX(frc::XboxController::kRightHand),0.0);
+		//Adding a new pneumatic function for potential climber or gear placement
+		//Work in Progress
+		pclXbox2->GetXButton() ? frc::DoubleSolenoid::Value::kForward : frc::DoubleSolenoid::Value::kReverse;
 	}
 
 	void TestPeriodic() {}
