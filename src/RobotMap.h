1bf252f6abb9a9042a30a8097c3a10f4097863da
bbaggerman <bob.github@baggerman.org>
2018-01-20 17:58:28 -0600

    A place to gather up all robot configuration settings

---
+++ src/RobotMap.h
@@ -0,0 +1,42 @@
+/* 2018 RobotMap.h */
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+
+#ifndef SRC_ROBOTMAP_H_
+#define SRC_ROBOTMAP_H_
+
+// Turn on or off various features
+// -------------------------------
+
+// NavX
+#define NAVX
+//#define JOYSTICK
+#define XBOX
+
+// CAN bus addresses
+// -----------------
+#define CAN_PDP					0
+#define CAN_TALON_LEFT_FRONT	1
+#define CAN_TALON_RIGHT_FRONT	2
+#define CAN_TALON_LEFT_REAR		3
+#define CAN_TALON_RIGHT_REAR	4
+#define CAN_TALON_ARM_MOTOR		5
+#define CAN_TALON_CLIMB_MOTOR	6
+#define CAN_PCM					11
+
+// PCM Channels
+#define PCM_CHAN_CLIMB_UP		0
+#define PCM_CHAN_ARM_DOWN		1
+#define PCM_CHAN_ARM_UP			3
+#define PCM_CHAN_ARM_DOWN		4
+
+// Digital I/O Channels
+#define DIO_LIMIT_SW			0
+
+
+#endif /* SRC_ROBOTMAP_H_ */
